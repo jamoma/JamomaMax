4d5d8eb10a4411f93ed9cbb986ca4e5dc1ec7bf5
Timothy Place <tim@electrotap.com>
2009-10-17 15:29:40 -0500

    {new newModule.rb: works with _template~ to create new audio modules and automates much of the initial work.}  It should work with other module types too, but templates for those types need to be made.

---
+++ newModule.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby -wKU
+
+# First include the functions in the jamoma lib
+glibdir = "."
+Dir.chdir glibdir             # change to libdir so that requires work
+glibdir = Dir.pwd
+require "support/jamomalib"
+
+
+###################################################################
+# Check Args
+###################################################################
+
+if(ARGV.length < 1)
+  puts "usage: "
+  puts "newModule.rb <required:newModuleName(no jmod. prefix)> <optional:moduleType{control(default), audio, spatialization, video, openGL}> <optional:path/to/newModuleParentFolder> "
+  puts "examples:"
+  puts "  ./newModule.rb tap.specialEffect~ audio /Users/tim/code/Jamoma/UserLib/Tap.Tools"
+  puts "  ./newModule.rb randomThing"
+  exit 0;
+end
+
+moduleName = ARGV[0]
+moduleType = "control"
+parentFolderPath = "#{glibdir}/../Modules/Modular/Jamoma/modules"
+
+moduleType = ARGV[1] if ARGV.length>1
+parentFolderPath = ARGV[2] if ARGV.length>2
+
+moduleFolder = "#{parentFolderPath}/#{moduleType}/#{moduleName}"
+
+templateName = "_template"
+templateName = "_template~"     if moduleType == "audio"
+templateName = "_sur.template~" if moduleType == "spatialization"
+templateName = "_template%"     if moduleType == "video"
+templateName = "_gl.template%"  if moduleType == "openGL"
+templateFolder = "#{glibdir}/../Modules/Modular/Jamoma/modules/#{moduleType}/#{templateName}"
+
+
+###################################################################
+# COPY
+
+puts `mkdir -pv "#{moduleFolder}"`
+puts `cp -v "#{templateFolder}"/* "#{moduleFolder}"`
+
+puts `mv "#{moduleFolder}"/jalg.#{templateName}.maxpat  "#{moduleFolder}"/jalg.#{moduleName}.maxpat`
+puts `mv "#{moduleFolder}"/jmod.#{templateName}.maxpat  "#{moduleFolder}"/jmod.#{moduleName}.maxpat`
+puts `mv "#{moduleFolder}"/jmod.#{templateName}.maxhelp "#{moduleFolder}"/jmod.#{moduleName}.maxhelp`
+puts `mv "#{moduleFolder}"/jmod.#{templateName}.xml     "#{moduleFolder}"/jmod.#{moduleName}.xml`
+puts `mv "#{moduleFolder}"/jmod.#{templateName}.html    "#{moduleFolder}"/jmod.#{moduleName}.html`
+
+
+###################################################################
+# SUBSTITUTIONS
+
+def substituteStringsOnFile (filepath, oldString, newString)
+  if FileTest.exist?(filepath)
+    f = File.open("#{filepath}", "r+")
+    str = f.read
+
+    str.gsub!(oldString, newString)
+
+    f.rewind
+    f.write(str)
+    f.truncate(f.pos)
+    f.close
+  end
+end
+
+substituteStringsOnFile("#{moduleFolder}/jalg.#{moduleName}.maxpat",  "#{templateName}", "#{moduleName}")
+substituteStringsOnFile("#{moduleFolder}/jmod.#{moduleName}.maxpat",  "#{templateName}", "#{moduleName}")
+substituteStringsOnFile("#{moduleFolder}/jmod.#{moduleName}.maxhelp", "#{templateName}", "#{moduleName}")
+substituteStringsOnFile("#{moduleFolder}/jmod.#{moduleName}.xml",     "#{templateName}", "#{moduleName}")
+substituteStringsOnFile("#{moduleFolder}/jmod.#{moduleName}.html",    "#{templateName}", "#{moduleName}")
+
