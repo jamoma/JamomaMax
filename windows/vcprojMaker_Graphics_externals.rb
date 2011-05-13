327f475b6c2c4cd66c9de21ba8f08350f32323b9
adrian_gierakowski <Adrian@Adrian_win7.(none)>
2011-05-13 11:22:24 +0100

    adding scripts for creating vcproj files for externals

---
+++ windows/vcprojMaker_Graphics_externals.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby -wKU
+
+require "FileUtils"
+
+glibdir = "."
+Dir.chdir glibdir             # change to libdir so that requires work
+glibdir = Dir.pwd
+
+Dir.chdir "#{glibdir}/../.."
+
+search = `ls -pRA | grep Modules/Graphics/.*MaxMSP.*xcode`
+
+xcodeProjs = Array::new
+xcodeProjs = search.split(":\n")
+
+created = 0
+
+xcodeProjs.each do |proj|
+  vcproj = proj.sub(/(.+)(.xcodeproj)/, '\1.vcproj')
+
+	unless File.exist?("#{vcproj}")
+		puts "CREATING MSVC PROJECT:  " + vcproj + "\n"
+		`cp ./Modules/Graphics/implementations/MaxMSP/jcom.label/jcom.label.vcproj #{vcproj}`
+		created += 1
+	end
+end
+puts "NOTHING TO DO!" if (created == 0)
