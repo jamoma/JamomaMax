ba0b6479c6e461231af26dfecfae08eb20cd88f8
@jln_ <julien.rabin@gmail.com>
2011-08-04 10:05:03 +0200

    initial fixe for managment of branches in update.rb script. Not closing this issue yet so more test and check is done. Seems to work though

--- update.rb
+++ update.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby -wKU
 
+# Usage:
+# "update.rb -> update all modules and submodules to their current branch"
+# "update.rb <branch> -> update main Jamoma module to <branch> and all submodules to their current branch"
+# "update.rb <branch> all -> update main Jamoma repository + submodules to <branch>"
+
+
 @glibdir = "."
 Dir.chdir @glibdir             # change to libdir so that requires work
 @glibdir = Dir.pwd
@@ -9,9 +15,8 @@ Dir.chdir @glibdir             # change to libdir so that requires work
 @remote.strip!
 
 # argument specifies branch (or meta-branch), otherwise default as below
-if ARGV.length > 0
-  @branch = ARGV[0]
-else
+if ARGV.length == 0
+  @all = false
   result = `git branch`
   result.each_line { |line|
     if line.match(/\*.*/)
@@ -19,9 +24,27 @@ else
       @branch = line.strip
     end
   }
+  puts "JAMOMA UPDATING MODULES & SUBMODULES TO THEIR CURRENT BRANCH FROM remote: #{@remote}"
+
+elsif  ARGV.length == 1
+  @all = false
+  @branch = ARGV[0]
+  puts "JAMOMA UPDATING TO branch: #{@branch} FROM remote: #{@remote}"
+  puts "MY BRANCH IS #{@branch}"
+  
+
+elsif ARGV.length == 2
+  @all = true
+  @branch = ARGV[0]
+  puts "JAMOMA UPDATING MODULES & SUBMODULES TO branch: #{@branch} FROM remote: #{@remote}"  
+else
+  puts "\nTOO MANY ARGUMENTS. USAGE IS AS FOLLOW"
+  puts "update.rb -> update all modules and submodules to their current branch\n"
+  puts "update.rb <bnameOfBranch> -> update main Jamoma module to <nameOfBranch> and all submodules to their current branch\n"
+  puts "update.rb <nameOfBranch> all -> update main Jamoma repository + submodules to <nameOfBranch>\n\n"
+  exit 0;
 end
 
-puts "JAMOMA UPDATING TO branch: #{@branch}  FROM remote: #{@remote}"
 
 
 #################################################################
@@ -34,41 +57,77 @@ end
 
 #################################################################
 
-def doUpdateSimple name
+def doUpdateSimple name #TODO case @all
+  if @all == true
+    brch = @branch
+  else
+    brch = @allBranches["#{name}"]
+  end
+  
   puts "UPDATING: #{name}"
   Dir.chdir "#{@glibdir}/../#{name}"
 
-  result = `git checkout #{@branch}`
+  result = `git checkout #{brch}`
   if result.match(/error.*/)
     puts "NOTE: checking out tracking branch..."
-    result = `git checkout --track remotes/#{@remote}/#{@branch}`
+    result = `git checkout --track remotes/#{@remote}/#{brch}`
   end
 
-  puts `git pull #{@remote} #{@branch}`
+  puts `git pull #{@remote} #{brch}`
   puts `git submodule update --init;`
+  # oldBranch = @allBranches["#{name}"]
+  # `git checkout "#{oldBranch}"` unless oldBranch == @branch
 end
 
 #################################################################
 
 def doUpdateUserLib name
-  doUpdateSimple("UserLib/#{name}")
+  doUpdateSimple("#{name}")
 end
 
 #################################################################
 
 def doUpdate name
-  doUpdateSimple("Modules/#{name}")
-  Dir.chdir "#{@glibdir}/../Modules/#{name}"
+  doUpdateSimple("#{name}")
+  Dir.chdir "#{@glibdir}/../#{name}"
   puts `ruby update.rb`
   divider
 end
 
 #################################################################
 
+Dir.chdir "#{@glibdir}/.."
 
+# TODO probably smarter way to do this : git submodule foreach 'echo $path;git branch|grep \*.*'
+
+# We build a hash of all module + their current branch beforehand
+if @all == false
+  divider
+  puts "CACHING SUBMODULES ACTIVE BRANCH"
+  divider
+  
+  @allBranches = {}
+subModList = `git submodule foreach 'echo $path'`.split("\n")
+subModList.delete_if {|x| x =~ /^Entering/} # Here we filter out useless stuff printed by Git
+subModList.each do |jamomaModule|
+  Dir.chdir "#{@glibdir}/../#{jamomaModule}"
+  b = `git branch`
+  b.each_line do |line|
+    if line.match(/\*.*/)
+      line.gsub!(/\*/, " ")
+      b = line.strip
+      puts "#{jamomaModule} is on branch #{b}"
+      @allBranches["#{jamomaModule}"] = "#{b}"
+    end
+  end
+end
+end
+
+Dir.chdir "#{@glibdir}/.."
+
+divider
 
 puts "UPDATING MAIN REPOSITORY..."
-Dir.chdir "#{@glibdir}/.."
 
 divider
 
@@ -95,14 +154,14 @@ doUpdateSimple "Tools/supports"
 
 divider
 
-doUpdate "Foundation"
-doUpdate "DSP"
-doUpdate "Graphics"
-doUpdate "Graph"
-doUpdate "AudioGraph"
-doUpdate "Modular"
-doUpdate "Test"
-doUpdate "Plugtastic"
+doUpdate "Modules/Foundation"
+doUpdate "Modules/DSP"
+doUpdate "Modules/Graphics"
+doUpdate "Modules/Graph"
+doUpdate "Modules/AudioGraph"
+doUpdate "Modules/Modular"
+doUpdate "Modules/Test"
+doUpdate "Modules/Plugtastic"
 
 doUpdateSimple "Modules/Documentation"
 doUpdateSimple "Modules/Dependencies"
@@ -130,7 +189,7 @@ ulModules = Array::new
 ulModules = ulDirContent.split("\n")
 
 ulModules.each do | ulGitDir |
-  doUpdateUserLib(ulGitDir) unless ulGitDir == "Orphans"
+  doUpdateUserLib("UserLib/#{ulGitDir}") unless ulGitDir == "Orphans"
 end
 
 divider
