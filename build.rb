1fe7a4d7172cd432566cf607354d91e83110980f
+1m place <tim@74objects.com>
2012-12-28 10:58:58 -0600

    build script tweak

--- build.rb
+++ build.rb
@@ -23,12 +23,13 @@ require "#{glibdir}/../../Core/Shared/jamomalib.rb"
 create_logs(@projectName)
 zero_count
 
-load "build.rb"
-
 `mkdir -p "#{glibdir}"/max/externals`
 `mkdir -p "#{glibdir}"/max/extensions`
 `rm -rf "#{glibdir}"/max/externals/*`
 `rm -rf "#{glibdir}"/max/extensions/*`
+
+load "build.rb"
+
 `cp -r "#{glibdir}"/../../Builds/MaxMSP/jcom.*.mxo "#{glibdir}"/max/externals`
 `cp -r "#{glibdir}"/max/externals/jcom.loader.* "#{glibdir}"/max/extensions`
 `cp -r "#{glibdir}"/../../Builds/MaxMSP/jcom.*.maxhelp "#{glibdir}"/max/help`
