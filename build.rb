15dde4d466c4486e56bd9ae546c1aef938e3082e
Trond Lossius <trond.lossius@bek.no>
2013-04-08 12:50:20 +0200

    We no longer copy files to Max5 and Max 6 folders in build script, as we now use the Max 6.1 Packages feature.

--- build.rb
+++ build.rb
@@ -91,48 +91,50 @@ Dir.chdir "#{glibdir}"
 
 if  win?
 else
-    
-    # Copy into Max 5 - comment this out if you don't want it
-    
-    puts "Copying various files to Max5 folder"
-    
-    `rm -rf "/Applications/Max5/Cycling '74/extensions/jcom.loader.mxo"`
-    `cp -r "Jamoma/extensions/jcom.loader.mxo" "/Applications/Max5/Cycling '74/extensions/jcom.loader.mxo"`
-    `cp -r "Jamoma/init/JamomaConfiguration.xml" "/Applications/Max5/Cycling '74/init/JamomaConfiguration.xml"`
-    
-    # Copy default files into Max 5 - comment this out if you don't want it
-    `cp -r "Jamoma/default-definitions/jcom.textslider.maxdefines" "/Applications/Max5/Cycling '74/default-definitions/jcom.textslider.maxdefines"`
-    `cp -r "Jamoma/default-definitions/jcom.ui.maxdefines" "/Applications/Max5/Cycling '74/default-definitions/jcom.ui.maxdefines"`
-    
-    `cp -r "Jamoma/default-settings/JamomaArarat.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaArarat.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaDark.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaDark.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaGraphite.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaGraphite.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaKulerBologna.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaKulerBologna.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaKulerQuietCry.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaKulerQuietCry.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaLight.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaLight.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaMax.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaMax.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaNoir.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaNoir.maxdefaults"`
-	
-	# Copy into Max 6 - comment this out if you don't want it
-    
-	puts "Copying various files to Max6 folder (this is no longer needed in Max 6.1)"
-	
-    `rm -rf "/Applications/Max6/Cycling '74/extensions/jcom.loader.mxo"`
-    `cp -r "Jamoma/extensions/jcom.loader.mxo" "/Applications/Max6/Cycling '74/extensions/jcom.loader.mxo"`
-    `cp -r "Jamoma/init/JamomaConfiguration.xml" "/Applications/Max6/Cycling '74/init/JamomaConfiguration.xml"`
+  puts
+  puts "We no longer copy files to Max 5 or Max 6 folders, as we now use the Max 6.1 Packages structure"
+  puts
+#    # Copy into Max 5 - comment this out if you don't want it
+#    
+#    puts "Copying various files to Max5 folder"
+#    
+#    `rm -rf "/Applications/Max5/Cycling '74/extensions/jcom.loader.mxo"`
+#    `cp -r "Jamoma/extensions/jcom.loader.mxo" "/Applications/Max5/Cycling '74/extensions/jcom.loader.mxo"`
+#    `cp -r "Jamoma/init/JamomaConfiguration.xml" "/Applications/Max5/Cycling '74/init/JamomaConfiguration.xml"`
+#    
+#    # Copy default files into Max 5 - comment this out if you don't want it
+#    `cp -r "Jamoma/default-definitions/jcom.textslider.maxdefines" "/Applications/Max5/Cycling '74/default-definitions/jcom.textslider.maxdefines"`
+#    `cp -r "Jamoma/default-definitions/jcom.ui.maxdefines" "/Applications/Max5/Cycling '74/default-definitions/jcom.ui.maxdefines"`
+#    
+#    `cp -r "Jamoma/default-settings/JamomaArarat.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaArarat.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaDark.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaDark.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaGraphite.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaGraphite.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaKulerBologna.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaKulerBologna.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaKulerQuietCry.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaKulerQuietCry.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaLight.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaLight.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaMax.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaMax.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaNoir.maxdefaults" "/Applications/Max5/Cycling '74/default-settings/JamomaNoir.maxdefaults"`
+#	
+#	# Copy into Max 6 - comment this out if you don't want it
+#    
+#	puts "Copying various files to Max6 folder (this is no longer needed in Max 6.1)"
+#	
+#    `rm -rf "/Applications/Max6/Cycling '74/extensions/jcom.loader.mxo"`
+#    `cp -r "Jamoma/extensions/jcom.loader.mxo" "/Applications/Max6/Cycling '74/extensions/jcom.loader.mxo"`
+#    `cp -r "Jamoma/init/JamomaConfiguration.xml" "/Applications/Max6/Cycling '74/init/JamomaConfiguration.xml"`
 	
 	# Copy default files into Max 6  - comment this out if you don't want it
-    `cp -r "Jamoma/default-definitions/jcom.textslider.maxdefines" "/Applications/Max6/Cycling '74/default-definitions/jcom.textslider.maxdefines"`
-    `cp -r "Jamoma/default-definitions/jcom.ui.maxdefines" "/Applications/Max6/Cycling '74/default-definitions/jcom.ui.maxdefines"`
-	
-    `cp -r "Jamoma/default-settings/JamomaArarat.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaArarat.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaDark.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaDark.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaGraphite.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaGraphite.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaKulerBologna.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaKulerBologna.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaKulerQuietCry.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaKulerQuietCry.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaLight.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaLight.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaMax.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaMax.maxdefaults"`
-    `cp -r "Jamoma/default-settings/JamomaNoir.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaNoir.maxdefaults"`
+#    `cp -r "Jamoma/default-definitions/jcom.textslider.maxdefines" "/Applications/Max6/Cycling '74/default-definitions/jcom.textslider.maxdefines"`
+#    `cp -r "Jamoma/default-definitions/jcom.ui.maxdefines" "/Applications/Max6/Cycling '74/default-definitions/jcom.ui.maxdefines"`
+#	
+#    `cp -r "Jamoma/default-settings/JamomaArarat.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaArarat.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaDark.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaDark.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaGraphite.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaGraphite.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaKulerBologna.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaKulerBologna.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaKulerQuietCry.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaKulerQuietCry.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaLight.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaLight.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaMax.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaMax.maxdefaults"`
+#    `cp -r "Jamoma/default-settings/JamomaNoir.maxdefaults" "/Applications/Max6/Cycling '74/default-settings/JamomaNoir.maxdefaults"`
     
 end
 
