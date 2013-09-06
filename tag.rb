042591ae6c58b693c0d5a75fc3e99813027cafe5
theod <theod@gmea.net>
2013-09-06 10:21:50 +0200

    Refactoring the tag script

--- tag.rb
+++ tag.rb
@@ -1,56 +1,56 @@
 #!/usr/bin/env ruby -wKU
 # encoding: utf-8
 
-# First include the functions in the jamoma lib
-glibdir = "../Modules"
-Dir.chdir glibdir             # change to libdir so that requires work
-glibdir = Dir.pwd
+# Go to Jamoma root folder
+rootfolder = "../"
+Dir.chdir rootfolder
+rootfolder = Dir.pwd
 
-$main_repository = true
-require "#{glibdir}/../Core/Shared/jamomalib"
+# Include the functions in the jamoma lib
+require "#{rootfolder}/Core/Shared/jamomalib"
 
-
-###################################################################
 # Check Args
-###################################################################
-
 if(ARGV.length == 0)
   puts "usage: "
-  puts "ruby tag.rb <required:tag-name>"
+  puts "ruby tag.rb <required:tag-name> <optional:branch-to-tag>"
   exit 0;
 end
 
 $tag = ARGV[0];
 
-def tagCore
-
-  puts "tagging Core"
-  `cd ../Core; git remote prune origin`
-  `cd ../Core; git tag #{$tag}`
-  `cd ../Core; git push origin --tags`
-
+if(ARGV.length == 2)
+  $branch = ARGV[1];
+else
+  $branch = "default";
 end
 
-def tagOne name
+# Define function to easily tag each sub modules
+def tagModule relativePathFromJamomaFolder
 
-  puts "tagging #{name}"
-  `cd ../Modules/#{name}; git remote prune origin`
-  `cd ../Modules/#{name}; git tag #{$tag}`
-  `cd ../Modules/#{name}; git push origin --tags`
-
+  if ($tag != "default")
+    puts "switching #{relativePathFromJamomaFolder} to #{$tag}"
+    `cd ./#{relativePathFromJamomaFolder}; git checkout #{$branch}`
+  end
+    
+  puts "tagging #{relativePathFromJamomaFolder}"
+  `cd ./#{relativePathFromJamomaFolder}; git remote prune origin`
+  `cd ./#{relativePathFromJamomaFolder}; git tag #{$tag}`
+  `cd ./#{relativePathFromJamomaFolder}; git push origin --tags`
 end
 
+# Tag the Jamoma repository
 `git remote prune origin`
 `git tag #{$tag}`
 `git push origin --tags`
 
-tagCore
-tagOne "Dependencies"
-tagOne "Documentation"
-tagOne "Modular"
-tagOne "Test"
-
-
-#{}`git submodule foreach git remote prune origin`
-#{}`git submodule foreach git tag #{tag}`
-#{}`git submodule foreach git push origin --tags`
+# Tag each sub modules
+tagModule "Core"
+tagModule "Documentation"
+tagModule "Implementations/AudioUnits"
+tagModule "Implementations/iOS"
+tagModule "Implementations/Max"
+tagModule "Implementations/MaxDependencies"
+tagModule "Implementations/MaxTest"
+tagModule "Implementations/PureData"
+tagModule "Implementations/Ruby"
+tagModule "Implementations/VST"
