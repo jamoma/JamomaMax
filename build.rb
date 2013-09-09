b3867c7033ed4dfbe88d6c2eefa0dcdf566d3eb4
laugre <laurent@blueyeti.fr>
2013-09-09 19:26:21 +0200

    script work on build folder (win)

--- build.rb
+++ build.rb
@@ -48,12 +48,12 @@ load "build.rb"
 # copy build results into the Max Package
 # todo : on windows add a case if we want debug or release, 32 or 64
 `cp -r "#{glibdir}"/../../Core/*/library/build/*.dylib "#{glibdir}"/Jamoma/support` if mac?
-`cp -r "#{glibdir}"/../../Core/*/library/Debug/*.dll "#{glibdir}"/Jamoma/support` if win?
+`cp -r "#{glibdir}"/../../Core/*/library/build/*.dll "#{glibdir}"/Jamoma/support` if win?
 `rm "#{glibdir}"/Jamoma/support/*-i386.dylib` if mac?
 `rm "#{glibdir}"/Jamoma/support/*-x86_64.dylib` if mac?
 `cp -r "#{glibdir}"/../../Core/*/extensions/*/build/*.ttdylib "#{glibdir}"/Jamoma/support` if mac?
-`cp -r "#{glibdir}"/../../Core/*/extensions/*/Debug/*.ttdll "#{glibdir}"/Jamoma/support` if win?
-`cp -r "#{glibdir}"/../../Core/*/extensions/*/Debug/*.dll "#{glibdir}"/Jamoma/support` if win?
+`cp -r "#{glibdir}"/../../Core/*/extensions/*/build/*.ttdll "#{glibdir}"/Jamoma/support` if win?
+`cp -r "#{glibdir}"/../../Core/*/extensions/*/build/*.dll "#{glibdir}"/Jamoma/support` if win?
 `rm "#{glibdir}"/Jamoma/support/*-i386.ttdylib` if mac?
 `rm "#{glibdir}"/Jamoma/support/*-x86_64.ttdylib` if mac?
 `mv "#{glibdir}"/Jamoma/externals/j.loader.mxo "#{glibdir}"/Jamoma/extensions/` if mac?
