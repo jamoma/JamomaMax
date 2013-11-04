d3d4111a3a211e2af0386b4b29e6311e76091b2d
Trond Lossius <trond.lossius@bek.no>
2013-11-04 13:23:58 +0100

    Fixing ruby indentation issue that would cause warning on OSX Maverick

--- build.rb
+++ build.rb
@@ -10,7 +10,7 @@
 
 # First include the functions in the jamoma lib
 glibdir = "../"
-Dir.chdir glibdir             # change to libdir so that requires work
+Dir.chdir glibdir						 # change to libdir so that requires work
 glibdir = Dir.pwd
 
 $main_repository = true
@@ -32,106 +32,104 @@ runTests = false
 # If no arguments are provided we post a help message
 
 ARGV.each do |arg|
-  if(arg=="help")
-    puts
-    puts "The build.rb script takes the following arguments:"
-    puts
-    puts "- <Deployment> or <Release> ensures a deployment build."
-    puts "  The default is a Development build"
-    puts
-    puts "- <clean> ensures a clean build."
-    puts "  The default is a non-clean build."
-    puts
-    puts "- <log> causes build logs to be posted to Terminal"
-    puts "  The default is that build logs are not posted to Terminal"
-    puts 
-    puts "- <test> causes Ruby unit tests to be run at the end of the build process"
-    puts "  The default is that unit tests are not run at the end of the build process"
-    puts
-#    puts "- Additionally on Mac you can enforce the use of a certain compiler"
-#    puts "  Possible options are <icc>, <gcc47> or <clang>"
-#    puts "  By default the build script will look for available compilers with the following priority:"
-#    puts "  (1) icc, (2) clang, (3) gcc47"
-#    puts
-    puts "The order of arguments is optional"
-    puts
-    exit 0
-  end
+	if(arg=="help")
+		puts
+		puts "The build.rb script takes the following arguments:"
+		puts
+		puts "- <Deployment> or <Release> ensures a deployment build."
+		puts "	The default is a Development build"
+		puts
+		puts "- <clean> ensures a clean build."
+		puts "	The default is a non-clean build."
+		puts
+		puts "- <log> causes build logs to be posted to Terminal"
+		puts "	The default is that build logs are not posted to Terminal"
+		puts 
+		puts "- <test> causes Ruby unit tests to be run at the end of the build process"
+		puts "	The default is that unit tests are not run at the end of the build process"
+		puts
+#		puts "- Additionally on Mac you can enforce the use of a certain compiler"
+#		puts "	Possible options are <icc>, <gcc47> or <clang>"
+#		puts "	By default the build script will look for available compilers with the following priority:"
+#		puts "	(1) icc, (2) clang, (3) gcc47"
+#		puts
+		puts "The order of arguments is optional"
+		puts
+		exit 0
+	end
 end
 
 ARGV.each do |arg|
-  
-  # Build Debug or Release?
-  if(arg=="Deployment" || arg=="Release" )
-    configuration = "Deployment"
-  end
-    
-  # Do a clean build?
-  if(arg == "clean")
-    clean = true
-  end
-  
-  # On the Mac you can force the use of a certain compiler
-  #if mac?
-  #  if(arg=="icc")
-  #    compiler = "icc"
-  #  elsif (arg=="gcc47")
-  #    compiler = "gcc47"
-  #  elsif (arg=="clang")
-  #    compiler = "clang"
-  #  end
-  #end
-  
-  # Do we want to post the log to Terminal?
-  if (arg=="log")
-    postLog = true
-  end
-  
-  # Do we want to run the tests at the ewnd of the building process?
-  if (arg=="test")
-    runTests = true
-  end
-  
+	
+	# Build Debug or Release?
+	if(arg=="Deployment" || arg=="Release" )
+		configuration = "Deployment"
+	end
+		
+	# Do a clean build?
+	if(arg == "clean")
+		clean = true
+	end
+	
+	# On the Mac you can force the use of a certain compiler
+	#if mac?
+	#	if(arg=="icc")
+	#		compiler = "icc"
+	#	elsif (arg=="gcc47")
+	#		compiler = "gcc47"
+	#	elsif (arg=="clang")
+	#		compiler = "clang"
+	#	end
+	#end
+	
+	# Do we want to post the log to Terminal?
+	if (arg=="log")
+		postLog = true
+	end
+	
+	# Do we want to run the tests at the ewnd of the building process?
+	if (arg=="test")
+		runTests = true
+	end
+	
 end
 
 # Having initially set all options we need to make some further Windows-specific precautions:
 
 if win?
- 	 if(configuration == "Development" || configuration == "Debug" )
-    		configuration = "Debug"
-  	else
-		  if(configuration == "Deployment" || configuration == "Release" )
-		    configuration = "Release"
-	    end
- 	 end
+ 	if(configuration == "Development" || configuration == "Debug" )
+				configuration = "Debug"
+	elsif(configuration == "Deployment" || configuration == "Release" )
+		configuration = "Release"
+ 	end
 end
 
 ###################################################################
 # We no longer install in /usr/local, so neither do we clean there
 ###################################################################
 #if(clean)
-#  
-#  if win?
-#    #windows specific loc. Where ?
-#  else
-#    if (File.exists? "/usr/local/jamoma/lib")
-#      puts "  Cleaning dylib folder "    
-#      Dir.chdir "/usr/local/jamoma/lib"
-#      `rm -rf *.*`
-#    end
-#    
-#    if (File.exists? "/usr/local/jamoma/extensions")
-#      puts "  Cleaning Extensions folder "    
-#      Dir.chdir "/usr/local/jamoma/extensions"
-#      `rm -rf *.*`
-#    end
-    
-#    if (File.exists? "/usr/local/jamoma/includes")
-#      puts "  Cleaning Include Header folder "
-#      Dir.chdir "/usr/local/jamoma/includes"
-#      `rm -rf *.*`
-#    end
-#  end
+#	
+#	if win?
+#		#windows specific loc. Where ?
+#	else
+#		if (File.exists? "/usr/local/jamoma/lib")
+#			puts "	Cleaning dylib folder "		
+#			Dir.chdir "/usr/local/jamoma/lib"
+#			`rm -rf *.*`
+#		end
+#		
+#		if (File.exists? "/usr/local/jamoma/extensions")
+#			puts "	Cleaning Extensions folder "		
+#			Dir.chdir "/usr/local/jamoma/extensions"
+#			`rm -rf *.*`
+#		end
+		
+#		if (File.exists? "/usr/local/jamoma/includes")
+#			puts "	Cleaning Include Header folder "
+#			Dir.chdir "/usr/local/jamoma/includes"
+#			`rm -rf *.*`
+#		end
+#	end
 #end
 
 
@@ -157,19 +155,19 @@ version_min = version_digits[1] if version_digits.size() > 1
 version_maj = version_digits[0] if version_digits.size() > 0
 
 puts
-puts "  Building Jamoma #{git_tag} (rev. #{git_rev})"
+puts "	Building Jamoma #{git_tag} (rev. #{git_rev})"
 puts
-puts "  configuration = #{configuration}"
-puts "  clean         = #{clean}"
-# puts "  compiler      = #{compiler}"
-puts "  postLog       = #{postLog}"
-puts "  runTests      = #{runTests}"
+puts "	configuration = #{configuration}"
+puts "	clean				 = #{clean}"
+# puts "	compiler			= #{compiler}"
+puts "	postLog			 = #{postLog}"
+puts "	runTests			= #{runTests}"
 puts
 if git_dirty_commits != '0'
-	puts "  !!! WARNING !!!"
-	puts "	THIS BUILD IS COMING FROM A DIRTY REVISION   "
-	puts "	THIS BUILD IS FOR PERSONAL USE ONLY  "
-	puts "	DO NOT DISTRIBUTE THIS BUILD TO OTHERS       "
+	puts "	!!! WARNING !!!"
+	puts "	THIS BUILD IS COMING FROM A DIRTY REVISION	 "
+	puts "	THIS BUILD IS FOR PERSONAL USE ONLY	"
+	puts "	DO NOT DISTRIBUTE THIS BUILD TO OTHERS			 "
 	puts ""
 end
 puts ""
@@ -179,9 +177,9 @@ puts ""
 ###################################################################
 
 quietly do
-#  ARGV = [configuration, clean, compiler, git_tag, git_rev]
-#  ARGV = [configuration, clean, compiler]
-  ARGV = [configuration, clean]
+#	ARGV = [configuration, clean, compiler, git_tag, git_rev]
+#	ARGV = [configuration, clean, compiler]
+	ARGV = [configuration, clean]
 end
 
 # Get a list of implementations that need to be built
@@ -193,51 +191,51 @@ load "build.rb"
 
 # Build everything in the 'Implementations' folder
 implementations.each {|implementation| 
-  if implementation[0] != '.' && File.exists?("#{glibdir}/Implementations/#{implementation}/build.rb")
-    Dir.chdir "#{glibdir}/Implementations/#{implementation}"
-    load "build.rb"
-  end
+	if implementation[0] != '.' && File.exists?("#{glibdir}/Implementations/#{implementation}/build.rb")
+		Dir.chdir "#{glibdir}/Implementations/#{implementation}"
+		load "build.rb"
+	end
 }
 
 # Install Ruby Support
 if win?
-  puts
-  puts "At the moment we don't build TTRuby for Windows"
-  puts "If someone is interested in doing that, please feel free!"
-  puts
+	puts
+	puts "At the moment we don't build TTRuby for Windows"
+	puts "If someone is interested in doing that, please feel free!"
+	puts
 else
-  puts
-  puts "Installing Jamoma Ruby support -- this may require your password"
-  puts "If you just press enter without entering your password, then the updated Jamoma Ruby support will not be installed for your use."
-  Dir.chdir "#{glibdir}/Implementations/Ruby"
-  load "install.rb"
+	puts
+	puts "Installing Jamoma Ruby support -- this may require your password"
+	puts "If you just press enter without entering your password, then the updated Jamoma Ruby support will not be installed for your use."
+	Dir.chdir "#{glibdir}/Implementations/Ruby"
+	load "install.rb"
 end
 
 
 if (runTests)
-  puts
-  puts "Running Unit Tests for all subprojects"
-  puts
-  submodules = Dir.entries("#{glibdir}/Core")
-  submodules.each {|submodule| 
-    if submodule[0] != '.' && File.exists?("#{glibdir}/Core/#{submodule}/test.rb") && File.directory?("#{glibdir}/Core/#{submodule}/Tests/unit")
-      Dir.chdir "#{glibdir}/Core/#{submodule}"
-      load "test.rb"
-    end
-  }
+	puts
+	puts "Running Unit Tests for all subprojects"
+	puts
+	submodules = Dir.entries("#{glibdir}/Core")
+	submodules.each {|submodule| 
+		if submodule[0] != '.' && File.exists?("#{glibdir}/Core/#{submodule}/test.rb") && File.directory?("#{glibdir}/Core/#{submodule}/Tests/unit")
+			Dir.chdir "#{glibdir}/Core/#{submodule}"
+			load "test.rb"
+		end
+	}
 else
-  puts "Not running unit tests"
+	puts "Not running unit tests"
 end
 
 if (postLog)
-  puts
-  puts "==================== SUMMARY ===================="
-  puts "Combined Error log of all subprojects:"
-  puts
-  Dir.chdir "#{glibdir}/Core/Shared"
-  puts `cat logs-*/error*`
+	puts
+	puts "==================== SUMMARY ===================="
+	puts "Combined Error log of all subprojects:"
+	puts
+	Dir.chdir "#{glibdir}/Core/Shared"
+	puts `cat logs-*/error*`
 else
-  puts "Not posting error logs"
+	puts "Not posting error logs"
 end
 
 puts
