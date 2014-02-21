bef32fa945a9404798072c58362ff19c43846838
theod <theod@gmea.net>
2014-02-21 15:21:51 +0100

    Warning user if he chooses a size larger than 80 x 60 instead of avoiding such large size

--- source/j.dbap/j.dbap.cpp
+++ source/j.dbap/j.dbap.cpp
@@ -470,7 +470,11 @@ void dbap_view_update(t_dbap *x, long io)
 /** Set the size of hitmap view window */
 void dbap_view_size(t_dbap *x, long sizeX, long sizeY) {
 	
-	if ((sizeX > 0)&&(sizeY > 0)&&(sizeX <= MAX_SIZE_VIEW_X)&&(sizeY <= MAX_SIZE_VIEW_Y)) {
+	if ((sizeX > 0)&&(sizeY > 0)) {
+        
+        // warn the user that large dimension while take a long to render
+        if ((sizeX > MAX_SIZE_VIEW_X)||(sizeY > MAX_SIZE_VIEW_Y))
+            object_warn(ObjectPtr(x), "size over %d x %d takes time to render", MAX_SIZE_VIEW_X, MAX_SIZE_VIEW_Y);
 		
 		x->view_info.dim[0] = sizeX;
 		x->view_info.dim[1] = sizeY;
