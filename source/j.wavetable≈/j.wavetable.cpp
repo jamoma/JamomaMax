a96eb215d6427e17fa652d76296ebe401521e42e
nwolek <nw@nathanwolek.com>
2014-03-02 17:32:53 +0100

    enabling OscilSetMode to accept a list so that filePath mode can load a sound file

--- source/j.wavetable≈/j.wavetable.cpp
+++ source/j.wavetable≈/j.wavetable.cpp
@@ -188,9 +188,16 @@ TTErr OscilDropAudio(OscilPtr self, long inletNumber, ObjectPtr sourceMaxObject,
 
 MaxErr OscilSetMode(OscilPtr self, void* attr, AtomCount argc, AtomPtr argv)
 {
+	
 	if (argc) {
+		TTValue v;
+		
+		v.setSize(argc);
+		for (int i=0; i<argc; i++)
+				v.set(i, atom_getfloat(argv+i));
+		
 		self->attrWaveform = atom_getsym(argv);
-		self->audioGraphObject->getUnitGenerator()->setAttributeValue(TT("mode"), TT(self->attrWaveform->s_name));
+		self->audioGraphObject->getUnitGenerator()->setAttributeValue(TT("mode"), v);
 	}
 	return MAX_ERR_NONE;
 }
