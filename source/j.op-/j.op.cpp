d3f3004c95483fc58f310dd9e5132d9996e42c48
Trond Lossius <trond.lossius@bek.no>
2014-05-22 18:18:41 +0200

    Adding Xcode project and removing one deprecation warning for j.op- #622

--- source/j.op-/j.op.cpp
+++ source/j.op-/j.op.cpp
@@ -89,8 +89,8 @@ OpPtr OpNew(t_symbol* msg, long argc, t_atom* argv)
 		self->graphOutlets[0] = outlet_new(self, "graph.connect");
 		
 		v.resize(2);
-		v.set(0, TT("operator"));
-		v.set(1, TTUInt32(1));
+		v[0] = "operator";
+		v[1] = 1;
 		err = TTObjectBaseInstantiate(TT("graph.object"), (TTObjectBasePtr*)&self->graphObject, v);
 
 		if (!self->graphObject->mKernel) {
