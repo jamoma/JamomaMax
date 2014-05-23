aa07672660450bffdbd3cb95c11218617e11a798
Trond Lossius <trond.lossius@bek.no>
2014-05-23 11:58:48 +0200

    j.=: Removing compilation errors and deprecation warnings, but object is not building yet.

--- source/j=/j=.cpp
+++ source/j=/j=.cpp
@@ -16,8 +16,8 @@
 #include "maxAudioGraph.h"
 
 // Prototypes for methods
-ObjectPtr	jamoma_new(SymbolPtr s, long argc, t_atom* argv);
-ObjectPtr	MaxAudioGraphWrappedClass_new(SymbolPtr name, long argc, t_atom* argv);
+t_object*	jamoma_new(t_symbol Æs, long argc, t_atom* argv);
+t_object*	MaxAudioGraphWrappedClass_new(t_symbol *name, long argc, t_atom* argv);
 
 // Globals
 static t_class*		s_jamoma_class;
@@ -42,12 +42,12 @@ int TTCLASSWRAPPERMAX_EXPORT main(void)
 // Then we wrap that class as a Max class.
 // Finally, we proceed to then instantiate the new Max class instead of the jamoma~ Max class.
 
-ObjectPtr jamoma_new(SymbolPtr s, long argc, t_atom* argv)
+t_object* jamoma_new(t_symbol *s, long argc, t_atom* argv)
 {
 	int								attrstart = attr_args_offset(argc, argv);
 	int								i = 0;
 	int								channelCount = 2;
-	SymbolPtr						className = gensym("gain");
+	t_symbol						*className = gensym("gain");
 	MaxAudioGraphWrappedClassPtr	classWrapper = NULL;
 	char							maxClassName[256];
 
