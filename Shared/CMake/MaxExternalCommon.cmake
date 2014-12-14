5c2e052c028391bb1f7fd65c5209bff5c9aa2b4a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2014-12-14 11:32:46 +0100

    Fix build of externals

--- Shared/CMake/MaxExternalCommon.cmake
+++ Shared/CMake/MaxExternalCommon.cmake
@@ -3,15 +3,10 @@ set(PROJECT_SRCS
 	${CMAKE_CURRENT_SOURCE_DIR}/../c74support/max-includes/common/commonsyms.cpp
 )
 
-include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/Foundation/library/includes")
-include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/DSP/library/includes")
-include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/Modular/library/includes")
 include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/Modular/library/PeerObject")
 include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/Modular/library/ModularForMax")
 include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/Modular/library/SchedulerLib")
 include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/Modular/library/ProtocolLib")
-include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/Graph/library/includes")
-include_directories("${PROJECT_SOURCE_DIR}/../../../../Core/AudioGraph/library/includes")
 include_directories("${PROJECT_SOURCE_DIR}/../classwrapper")
 include_directories("${FILE_H_DIR}")
 
@@ -19,12 +14,12 @@ addMaxsupport()
 
 add_library(${PROJECT_NAME} SHARED ${PROJECT_SRCS})
 
-target_link_libraries(${PROJECT_NAME} JamomaFoundation)
-target_link_libraries(${PROJECT_NAME} JamomaModular)
+target_link_libraries(${PROJECT_NAME} Foundation)
+target_link_libraries(${PROJECT_NAME} Modular)
 target_link_libraries(${PROJECT_NAME} JamomaMax)
-target_link_libraries(${PROJECT_NAME} JamomaDSP)
-target_link_libraries(${PROJECT_NAME} JamomaGraph)
-target_link_libraries(${PROJECT_NAME} JamomaAudioGraph)
+target_link_libraries(${PROJECT_NAME} DSP)
+target_link_libraries(${PROJECT_NAME} Graph)
+target_link_libraries(${PROJECT_NAME} AudioGraph)
 
 ### Output ###
 setOutput()
