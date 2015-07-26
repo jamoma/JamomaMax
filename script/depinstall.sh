80a286add568b2460d9827dbc53bf9ce72083128
Antoine Villeret - Ubuntu 12.04 server <antoine.villeret@gmail.com>
2015-07-27 00:06:52 +0200

    fix CMake installation

--- script/depinstall.sh
+++ script/depinstall.sh
@@ -6,7 +6,8 @@ cd ${HOME}
 case "$TRAVIS_OS_NAME" in
   linux)
       wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
-      tar -xzf cmake-3.2.2-Linux-x86_64.tar.gz -C ${HOME}/cmake --strip-components=1
+      tar -xzf cmake-3.2.2-Linux-x86_64.tar.gz
+      mv cmake-3.2.2-Linux-x86_64 cmake
 
       git clone https://github.com/avilleret/mingw-w64-build ${HOME}/mingw-w64-install
 	 ;;
