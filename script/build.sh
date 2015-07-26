35da1c0ed91d7c4459f513d7b1c96bcf994e3b80
Antoine Villeret - Ubuntu 12.04 server <antoine.villeret@gmail.com>
2015-07-27 00:02:20 +0200

    build even if we are on feature/mingw-w64 branch

--- script/build.sh
+++ script/build.sh
@@ -9,11 +9,6 @@ if [ "x${COVERITY_SCAN_BRANCH}" = "x1" ]; then
   exit 0
 fi
 
-if [ "x${TRAVIS_BRANCH}" = "xfeature/mingw-w64" -a "x${ARCH}" != "xmingw-w64" ]; then
-  echo "We are on feature/mingw-w64 branch, don't build for other arch"
-  exit 0
-fi
-
 mkdir -p build
 cd build
 
