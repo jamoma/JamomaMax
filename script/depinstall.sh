c3b88d3f44e70903ca53dd6cf1eff36672e83168
Antoine Villeret - Ubuntu 12.04 server <antoine.villeret@gmail.com>
2015-07-27 15:07:48 +0200

    fix cmake path on OSX

--- script/depinstall.sh
+++ script/depinstall.sh
@@ -10,12 +10,14 @@ case "$TRAVIS_OS_NAME" in
       mv cmake-3.2.2-Linux-x86_64 cmake
 
       git clone https://github.com/avilleret/mingw-w64-build ${HOME}/mingw-w64-install
-	 ;;
-	osx)
-			wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Darwin-x86_64.tar.gz
-			tar -xf cmake-3.2.2-Darwin-x86_64.tar.gz -C ${HOME}/cmake --strip-components=1
-			ln -s ${HOME}/CMake.app/Contents/bin ${HOME}/cmake/bin
+   ;;
+  osx)
+      wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Darwin-x86_64.tar.gz
+      tar -xf cmake-3.2.2-Darwin-x86_64.tar.gz
+      mkdir -p cmake
+      ln -s ${HOME}/cmake-3.2.2-Darwin-x86_64/CMake.app/Contents/bin ${HOME}/cmake/
 
       brew install coreutils
-	;;
+  ;;
 esac
+cd -
