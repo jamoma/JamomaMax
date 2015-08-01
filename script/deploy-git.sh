b15c882569c95ed859fe52bb44e433e330e49e7b
Antoine Villeret <antoine.villeret@gmail.com>
2015-08-01 18:41:51 +0200

    fix OSX archive creation

--- script/deploy-git.sh
+++ script/deploy-git.sh
@@ -62,7 +62,7 @@ fi
 cd ${TRAVIS_BUILD_DIR}/build
 cmake -DCMAKE_INSTALL_COMPONENT=JamomaMax -P cmake_install.cmake
 
-cd ${TRAVIS_BUILD_DIR}/pd-package
+cd ${TRAVIS_BUILD_DIR}/JamomaInstall/JamomaMax/
 tar cvzf "${TRAVIS_BUILD_DIR}/${ARCHIVE_NAME}" Jamoma/
 
 cd ${TRAVIS_BUILD_DIR}
