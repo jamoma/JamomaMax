7fb2c53beac9c4b0f2ff3d7393a6e29634362c8d
theod <theo.delahogue@gmea.net>
2015-08-19 15:50:03 +0200

    Raising MAX_NUM_SOURCES and MAX_NUM_DESTINATIONS to 1024. Fixing #778.

--- source/j.dbap/j.dbap.h
+++ source/j.dbap/j.dbap.h
@@ -15,9 +15,8 @@
 #include "jit.common.h"
 #include "hull2D.h"
 
-// This seems to be the current restrictions of matrix~
-const long MAX_NUM_SOURCES = 128;
-const long MAX_NUM_DESTINATIONS = 128;
+const long MAX_NUM_SOURCES = 1024;
+const long MAX_NUM_DESTINATIONS = 1024;
 
 const long MAX_NUM_WEIGHTED_SOURCES = 64;
 const long MAX_NUM_WEIGHTED_DESTINATIONS = 32;
